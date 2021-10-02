e7d2594073f28d8379e643c1cfe2f9d06d1f34d8
MamoruDS <mamoruds.io@gmail.com>
2021-10-02 15:17:35 +0900

    added: implementation for method parse

--- typed_cap/cap.py
+++ typed_cap/cap.py
@@ -9,6 +9,7 @@ from typing import (
     Generic,
     List,
     Optional,
+    Tuple,
     Type,
     TypeVar,
     TypedDict,
@@ -89,8 +90,62 @@ class Cap(Generic[K, T, U]):
         for arg, opt in helpers.items():
             self._args[arg] = {**self._args[arg], **opt}  # type: ignore[misc]
 
-    def parse(self, args: List[str] = sys.argv[1:]) -> T:  # type: ignore
-        out = utils.args_parser(args, [])
-        for k, v in out.items():
-            # TODO:
-            pass
+    def parse(self, args: List[str] = sys.argv[1:]) -> T:
+        def find_key(name: str) -> str:
+            for key, opt in self._args.items():
+                if key == name:
+                    return key
+                if opt["alias"] == name:
+                    return key
+            raise Exception  # TODO:
+
+        def extract_list_type(t: str) -> Tuple[bool, str]:
+            if t == "list":
+                return True, "str"
+            reg = re.compile(r"^(list\[(?P<type>\w+)\])$")
+            m = reg.match(t)
+            if m != None:
+                return True, m.group("type")
+            else:
+                return False, t
+
+        flags = []
+        for key, opt in self._args.items():
+            if opt["type"] == "bool":
+                flags.append(key)
+                if opt["alias"] != None:
+                    flags.append(opt["alias"])
+
+        out = utils.args_parser(args, flags)
+
+        for key, val in out["options"].items():
+            opt = self._args[find_key(key)]
+            is_list, t = extract_list_type(opt["type"])
+            _val: List[Any]
+            if is_list:
+                _val = val
+            else:
+                _val = val[:1]
+            for i, v in enumerate(val):
+                if t == "str":
+                    _val[i] = v
+                if t == "int":
+                    _val[i] = int(v)
+                if t == "float":
+                    _val[i] = float(v)
+                if t == "bool":
+                    _val[i] = bool(v)
+            if is_list:
+                self._args[find_key(key)]["val"] = val
+            else:
+                self._args[find_key(key)]["val"] = val[0]
+
+        res: Dict[str, Any] = {"_": out["args"]}
+        for key, opt in self._args.items():
+            if key == "_":
+                continue
+            res[key] = opt["val"]
+            if res[key] == None and not opt["optional"]:
+                # TODO:
+                raise Exception(f"missing option: {key}")
+        return res  # type: ignore
