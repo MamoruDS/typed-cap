a9b8cc3c7ea19251d70f4b4794fca061c8dd6272
MamoruDS <mamoruds.io@gmail.com>
2022-03-03 00:29:26 +0900

    added: methods `is_T_based`

--- typed_cap/utils.py
+++ typed_cap/utils.py
@@ -1,3 +1,4 @@
+import inspect
 import re
 import shutil
 from sys import stderr
@@ -6,8 +7,10 @@ from typing import (
     List,
     NoReturn,
     Optional,
+    Type,
     TypeVar,
     Union,
+    _TypedDictMeta,  # type: ignore
 )
 
 
@@ -22,6 +25,25 @@ def get_terminal_width(max_width: int) -> int:
     s = shutil.get_terminal_size((999, 999))
     w = s.columns
     return min(w, max_width)
+
+
+# FIXME: potential issues
+def is_T_based(x) -> Union[Type[dict], Type[object], None]:
+    if not inspect.isclass(x):
+        return None
+    if type(x) is _TypedDictMeta:
+        return dict
+    b = x.__base__
+    while True:
+        if b is dict:
+            return dict
+        elif b is object:
+            return object
+        elif b is None:
+            break
+        else:
+            b = b.__base__
+    return None
 
 
 def join(a: List[str], j: str) -> str:
