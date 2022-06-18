659559eae4714aca78e69966ab28998bf59c5b26
MamoruDS <mamoruds.io@gmail.com>
2022-06-19 04:44:16 +0900

    added: two new cmt_params: `@delimiter` and `@none_delimiter`

--- typed_cap/cmt_param.py
+++ typed_cap/cmt_param.py
@@ -1,11 +1,13 @@
 from typed_cap.types import (
     ArgOption,
-    CmtParamInvalidAlias,
     VALID_ALIAS_CANDIDATES,
     CmtParamInvalidFlagValue,
+    CmtParamInvalidValue,
     CmtParamMissingValue,
 )
+from typed_cap.utils import RO
 from typing import Dict, NoReturn, Optional, TypedDict, Union, get_args
+
 
 _CmtParamVal = Optional[str]
 
@@ -13,24 +15,30 @@ _CmtParamVal = Optional[str]
 class ValidParams(TypedDict, total=False):
     alias: VALID_ALIAS_CANDIDATES
     show_default: bool
+    delimiter: RO[str]
 
 
 NamedValidParams = Dict[str, ValidParams]
 
 
 def _parse_flag_generic(
-    name: str, flag_name: str, val: _CmtParamVal, flag_val: bool
+    name: str,
+    flag_name: str,
+    val: _CmtParamVal,
+    flag_val: bool,
+    allow_val: bool,
 ) -> Union[bool, NoReturn]:
     if val is None:
         return flag_val
-
-    if val in ["False", "True", "false", "true"]:
-        if val.lower() == "true":
-            return flag_val
-        else:
-            return not flag_val
-    else:
+    elif allow_val:
+        if val in ["False", "True", "false", "true"]:
+            if val.lower() == "true":
+                return flag_val
+            else:
+                return not flag_val
         raise CmtParamInvalidFlagValue(name, flag_name, val)
+    else:
+        raise CmtParamInvalidValue(name, flag_name, val, "no value required")
 
 
 def _parse_alias(
@@ -42,13 +50,50 @@ def _parse_alias(
     if val in get_args(VALID_ALIAS_CANDIDATES):
         return val  # type: ignore
     else:
-        raise CmtParamInvalidAlias(name, "alias", val)
+        raise CmtParamInvalidValue(name, "alias", val)
 
 
 def _parse_show_default(
     name: str, val: _CmtParamVal, flag_val: bool
 ) -> Union[bool, NoReturn]:
-    return _parse_flag_generic(name, "hide_result", val, flag_val)
+    return _parse_flag_generic(
+        name,
+        "show_result",
+        val,
+        flag_val,
+        allow_val=True,
+    )
+
+
+def _parse_hide_default(
+    name: str, val: _CmtParamVal, flag_val: bool
+) -> Union[bool, NoReturn]:
+    return _parse_flag_generic(
+        name,
+        "hide_result",
+        val,
+        flag_val,
+        allow_val=False,
+    )
+
+
+def _parse_delimiter(name: str, val: _CmtParamVal) -> Union[RO[str], NoReturn]:
+    if val is None:
+        raise CmtParamMissingValue(name, "delimiter")
+    return RO.Some(val)
+
+
+def _parse_none_delimiter(
+    name: str, val: _CmtParamVal
+) -> Union[RO[str], NoReturn]:
+    _parse_flag_generic(
+        name,
+        "none_delimiter",
+        val,
+        flag_val=True,
+        allow_val=False,
+    )
+    return RO.Some(None)
 
 
 def parse_anno_cmt_params(
@@ -63,13 +108,15 @@ def parse_anno_cmt_params(
             # @alias
             elif key == "alias":
                 params["alias"] = _parse_alias(name, val)
+
             # @hide_default
             elif key == "hide_default":
-                params["show_default"] = _parse_show_default(
+                params["show_default"] = _parse_hide_default(
                     name,
                     val,
                     flag_val=False,
                 )
+
             # @show_default
             elif key == "show_default":
                 params["show_default"] = _parse_show_default(
@@ -77,6 +124,18 @@ def parse_anno_cmt_params(
                     val,
                     flag_val=True,
                 )
+
+            # @delimiter
+            elif key == "delimiter":
+                params["delimiter"] = _parse_delimiter(name, val)
+
+            # @none_delimiter
+            elif key == "none_delimiter":
+                params["delimiter"] = _parse_none_delimiter(
+                    name,
+                    val,
+                )
+
         named_param[name] = params
     return named_param
 
