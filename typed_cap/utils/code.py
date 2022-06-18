7e673ede9eb64b213f44bdb437c7e5980ffb7f06
MamoruDS <mamoruds.io@gmail.com>
2022-06-18 22:28:42 +0900

    added: methods `get_annotations` and `get_all_comments_parameters`
    improved: method `get_doc_from_ast` with type checking
    
    renamed: `get_named_doc` -> `get_docs_from_annotations`

--- typed_cap/utils/code.py
+++ typed_cap/utils/code.py
@@ -2,7 +2,22 @@ import ast
 import inspect
 import re
 import sys
-from typing import Dict, Optional
+from typing import Any, Dict, Optional, TypedDict, Union
+from urllib.parse import parse_qsl, urlparse, parse_qs
+
+
+class _ParsedAnno:
+    lineno: int
+    doc: Optional[str] = None
+    comment: Optional[str] = None
+
+    def __init__(self, lineno: int) -> None:
+        self.lineno = lineno
+
+
+class AnnoDetail(TypedDict):
+    doc: Optional[str]
+    comment: Optional[str]
 
 
 def reset_indent(code: str) -> str:
@@ -19,27 +34,37 @@ def reset_indent(code: str) -> str:
 
 
 def get_doc_from_ast(
-    parsed: ast.Module, named_doc: Dict[str, str] = {}
+    parsed: Union[ast.Module, ast.ClassDef],
+    named_doc: Dict[str, _ParsedAnno] = {},
 ) -> None:
     last = None
     for i in parsed.body:
         try:
             if isinstance(i, ast.Expr):
                 if isinstance(last, ast.AnnAssign):
-                    name = last.target.id
-                    docs = i.value.value
-                    named_doc[name] = docs
+                    target: ast.Name = last.target  # type: ignore
+                    name = target.id
+                    if isinstance(i.value, ast.Constant):
+                        docs: str = i.value.value
+                        named_doc[name].doc = docs
             elif isinstance(i, ast.ClassDef):
                 get_doc_from_ast(i, named_doc)
-
+            elif isinstance(i, ast.AnnAssign):
+                target: ast.Name = i.target  # type: ignore
+                name = target.id
+                named_doc[name] = _ParsedAnno(i.lineno)
+            else:
+                continue
         except TypeError:
             ...
         last = i
 
 
-def get_named_doc(c: type, stop_at: Optional[type] = None) -> Dict[str, str]:
+def get_annotations(
+    c: type, stop_at: Optional[type] = None
+) -> Dict[str, AnnoDetail]:
     _stop_before = [dict, object]
-    named_doc: Dict[str, str] = {}
+    named_anno: Dict[str, AnnoDetail] = {}
 
     bases = []
     try:
@@ -56,27 +81,81 @@ def get_named_doc(c: type, stop_at: Optional[type] = None) -> Dict[str, str]:
         src = inspect.getsource(c)
         src = reset_indent(src)
         parsed = ast.parse(src)
-        get_doc_from_ast(parsed, named_doc)
 
-    for name, doc in named_doc.items():
-        doc = re.sub(
-            r"(\A\s+)|(^[\t| ]{0,})|([\t| ]{0,}$)",
-            "",
-            doc,
-            flags=re.M,
-        )
-        doc = re.sub(
-            r"(?<!^)\n([\w|-])",
-            r" \g<1>",
-            doc,
-            flags=re.M,
-        )
-        doc = re.sub(r"\n$", "", doc, flags=re.M)
-        doc = re.sub(
-            r"\s(-{3,})\s",
-            r"\n\g<1>\n",
-            doc,
-            flags=re.M,
-        )
-        named_doc[name] = doc
-    return named_doc
+        local_anno: Dict[str, _ParsedAnno] = {}
+        get_doc_from_ast(parsed, local_anno)
+
+        for name, anno in local_anno.items():
+            if named_anno.get(name) is None:
+                named_anno[name] = {
+                    "doc": None,
+                    "comment": None,
+                }
+            if anno.doc is not None:
+                named_anno[name]["doc"] = anno.doc
+            try:
+                comment = src.split("\n")[anno.lineno - 2].lstrip()
+                if comment.startswith("#"):
+                    print(f"... {comment}")
+                    named_anno[name]["comment"] = comment
+            except IndexError:
+                ...
+
+    return named_anno
+
+
+def get_docs_from_annotations(
+    annotations: Dict[str, AnnoDetail]
+) -> Dict[str, str]:
+    named_docs: Dict[str, str] = {}
+    for name, anno in annotations.items():
+        if anno["doc"] is not None:
+            doc = anno["doc"]
+            doc = re.sub(
+                r"(\A\s+)|(^[\t| ]{0,})|([\t| ]{0,}$)",
+                "",
+                doc,
+                flags=re.M,
+            )
+            doc = re.sub(
+                r"(?<!^)\n([\w|-])",
+                r" \g<1>",
+                doc,
+                flags=re.M,
+            )
+            doc = re.sub(r"\n$", "", doc, flags=re.M)
+            doc = re.sub(
+                r"\s(-{3,})\s",
+                r"\n\g<1>\n",
+                doc,
+                flags=re.M,
+            )
+            named_docs[name] = doc
+    return named_docs
+
+
+def get_all_comments_parameters(
+    annotations: Dict[str, AnnoDetail]
+) -> Dict[str, Dict[str, Any]]:
+    named_params: Dict[str, Dict[str, Any]] = {}
+    reg = re.compile(r"@(?P<key>\w+)(=(?P<val>((\\@)|([^@]))+))?", re.M)
+
+    for name, anno in annotations.items():
+        comment = anno["comment"]
+        params: Dict[str, Any] = {}
+
+        if comment is not None:
+            comment = comment[1:].lstrip()
+
+            for match in reg.finditer(comment):
+                key = match.group("key")
+                val = match.group("val")
+
+                if val is None:
+                    val = True
+                else:
+                    val = val.rstrip()
+                params[key] = val
+
+        named_params[name] = params
+    return named_params
